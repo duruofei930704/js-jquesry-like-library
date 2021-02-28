99cadbfee4d15d9ad7c4c46ace985c721d10875e
Ruofei Du <dur@myumanitoba.ca>
2021-02-28 16:58:46 -0500

    implemented and tested library jsLib

---
+++ jsLib.js
@@ -0,0 +1,23 @@
+function $(selector){
+  const self = {
+    element: document.querySelector(selector),
+    html: ()=> self.element,
+    on:(event, callback)=>{
+      self.element.addEventListener(event, callback)
+    },
+    hide: ()=>{
+      self.element.style.display = "none"
+    },
+    attr: (name, value)=>{
+      if (value==null){
+        self.element.getAttribute(name);
+      }else{
+        self.element.setAttribute(name, value);
+      }
+    }
+    
+  }
+  return self;
+}
+
+export default $;
---
+++ script.js
@@ -0,0 +1,7 @@
+import $ from './jsLib.js';
+
+
+$('h1').hide();
+console.log($('h2').html());
+console.log($('h3').element);
+$('h4').attr('class', 'hello');
